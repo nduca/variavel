0a143cec1a784bb2a1e4cdb622132d6a477aa4ad
Nathan Duca <nathan.duca@grupomult.com.br>
2023-08-11 16:49:27 -0300

    Commit inicial

---
+++ src/Variavel.java
@@ -0,0 +1,29 @@
+public class Variavel {
+
+    public static void main(String[] args){
+
+        int valorInt = 10;
+        Integer valorInteger = 50;
+
+        byte umByte = 1;
+        Byte valorByte = 100;
+
+        char var = 'a';
+        Character character = 'z';
+
+        boolean novo = false;
+        Boolean boo = true;
+
+        System.out.println("int: " + valorInt);
+        System.out.println("Integer: " + valorInteger);
+
+        System.out.println("byte: " + umByte);
+        System.out.println("Byte: " + valorByte);
+
+        System.out.println("char: " + var);
+        System.out.println("Character: " + character);
+
+        System.out.println("boolean " + novo);
+        System.out.println("Boolean " + boo);
+    }
+}
